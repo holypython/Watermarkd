5d75d7e11bd62c691cf179133c99426ffea5cf85
HolyPython <58428858+holypython@users.noreply.github.com>
2020-09-28 00:42:20 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,57 +6,36 @@
 
 """Simple Photo Watermarker"""
 
-from setuptools import setup, find_packages
-# from codecs import open
-import io
-from os import path
+import setuptools
+from setuptools import setup
 
-here = path.abspath(path.dirname(__file__))
-
-# Get the long description from the README file
-with io.open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+with open("README.md", 'r') as f:
     long_description = f.read()
 
 setup(
-    name='Watermarkd',
-    version="0.8",
-    description='Simple Photo Watermarker',
-    long_description=long_description,
-    url='https://github.com/holypython',
-    author='Holypython.com',
-    author_email='admin@holypython.com',
-    license='Apache',
-    classifiers=[
-        'License :: OSI Approved :: Apache Software License',
-        # 'Development Status :: 3 - Alpha',
-        # 'Development Status :: 4 - Beta',
-        'Development Status :: 5 - Production/Stable',
+   name='Watermarkd',
+   version='0.7.0.2',
+   description='A friendly watermarking tool with optional GUI component.',
+   license="Apache-2.0",
+   long_description=long_description,
+   author='holypython.com',
+   author_email='watermarkd@holypython.com',
+   url="https://holypython.com/",
+   download_url = '',
+   packages=['Watermarkd'],
+   keywords = ['watermarking', 'image processing', 'watermark', 'photography', 'copyrights', 'holypython', 'batch watermark', 'holypython.com'],
+   classifiers=[
+       "Development Status :: 3 - Alpha",
+       "Intended Audience :: Developers :: Photographers :: Bloggers :: Businesses :: Entrepreneurs",
+       "Programming Language :: Python :: 3",
+       "License :: OSI Approved :: Apache-2.0 License",
+       "Operating System :: OS Independent",
+   ],
 
+   install_requires=[
+       'pillow',
+       'pysimplegui',
+   ],
 
-        'Operating System :: OS Independent',
-        'Intended Audience :: Photographers',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Entrepreneurs',
-        'Topic :: Office/Business :: Digital Image Processing',
-        'Topic :: Office/Business :: Digital Image Processing :: Photography',
-        'Topic :: Software Development :: Libraries',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-    ],
-    platforms=['any'],
-    keywords='watermark, watermarking, photography, image processing, batch image processing, holypython, holypython.com',
-    packages=find_packages(exclude=['contrib', 'docs', 'tests', 'examples']),
-    install_requires=['pandas>=0.24', 'numpy>=1.15',
-                      'random>=x.xx', 'pysimplegui>=x.x.x',
-                      'PIL>=x.x.x'],
-    entry_points={
-        'console_scripts': [
-            'sample=sample:main',
-        ],
-    },
+   python_requires='>=3.6'
 )

d997a4b40f75d574a35c6d16bca643aab599125b
HolyPython <58428858+holypython@users.noreply.github.com>
2020-09-29 01:29:18 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,14 +14,14 @@ with open("README.md", 'r') as f:
 
 setup(
    name='Watermarkd',
-   version='0.7.1.1',
+   version='0.7.1.2',
    description='A friendly watermarking tool with optional GUI component.',
    license="Apache-2.0",
    long_description=long_description,
    author='holypython.com',
    author_email='watermarkd@holypython.com',
    url="https://holypython.com/",
-   download_url = 'https://github.com/holypython/Watermarkd/archive/0.7.1.1.tar.gz',
+   download_url = 'https://github.com/holypython/Watermarkd/archive/0.7.1.2.tar.gz',
    packages=['Watermarkd'],
    keywords = ['watermarking', 'image processing', 'watermark', 'photography', 'copyrights', 'holypython', 'batch watermark', 'holypython.com'],
    classifiers=[
